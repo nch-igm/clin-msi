99cf845f01ac852616619d9b61d3ff2cc53be39e
Patrick Brennan <patrick.brennan@nationwidechildrens.org>
2021-01-04 10:27:38 -0500

    IGMBFX-1274 script is now function

--- src/msi_read_count_v2.py
+++ src/msi_read_count_v2.py
@@ -2,6 +2,8 @@ import pysam
 import argparse
 import re
 from operator import itemgetter
+from collections import defaultdict
+import pandas as pd
 
 def clin_msi_argparser():
     parser = argparse.ArgumentParser(description="")
@@ -9,13 +11,15 @@ def clin_msi_argparser():
     parser.add_argument('--bam', type=str, required=True)
     parser.add_argument('--reference', type=str, required=True)
     parser.add_argument('--output-dir', type=str, required=True, help="")
+    parser.add_argument('--allow_mismatch', action='store_true')
 
     return parser
 
 def repetitions(s):
-   r = re.compile(r"(.+?)\1+")
-   for match in r.finditer(s):
-       yield (match.group(1), len(match.group(0))/len(match.group(1)))
+    #Taken from https://stackoverflow.com/questions/9079797/detect-repetitions-in-string
+    r = re.compile(r"(.+?)\1+")
+    for match in r.finditer(s):
+        yield (match.group(1), len(match.group(0))/len(match.group(1)))
 
 def parse_input_file(input_file):
     location_list = []
@@ -33,17 +37,16 @@ def clin_msi():
     msi_location_list = parse_input_file(args.input_file)
 
     bam_file = pysam.AlignmentFile(args.bam, "rb")
+    df = pd.DataFrame()
 
     for chr, start, stop in msi_location_list:
+        length_dict = defaultdict(int)
         fasta_seq = pysam.faidx(args.reference, f"{chr}:{start-10}-{stop+10}").split('\n')[1]
-        #fastq_seq = fastq_seq.split('\n')[1]
         rep_list = list(repetitions(fasta_seq))
         print(rep_list)
         largest_rep_unit = max(rep_list, key=itemgetter(1))[0]
         largest_rep_len = int(max(rep_list, key=itemgetter(1))[1])
         print(largest_rep_len, largest_rep_unit)
-        search_string = fr"(\w{{5}}){largest_rep_unit}{{{largest_rep_len}}}(\w{{5}})"
-        print(search_string)
         get_flanks = re.search(fr"(\w{{5}}){largest_rep_unit}{{{largest_rep_len}}}(\w{{5}})", fasta_seq)
         left_flank = get_flanks.group(1)
         right_flank = get_flanks.group(2)
@@ -62,29 +65,28 @@ def clin_msi():
                 continue
 
             difference = start - read.reference_start
+            #print(re_string)
+            read_wo_softclip = read.query_sequence[read.query_alignment_start:read.query_alignment_end]
+            get_rep = re.search(fr"{left_flank}({largest_rep_unit}+){right_flank}", read_wo_softclip)
+            if args.allow_mismatch:
+                get_rep = re.search(fr"{left_flank}({largest_rep_unit}+[ACTG]?{largest_rep_unit}+){right_flank}", read_wo_softclip)
+            if get_rep is None:
+                continue
+            length_dict[len(get_rep.group(1))] += 1
 
-            #print(read.mapping_quality)
-            #print(read)
-            #print(read.query_sequence)
-            read_start = read.query_alignment_start + difference
-            #print(read.query_sequence[read_start:read.query_alignment_end])
-            #print(list(repetitions(read.query_sequence[read_start:read.query_alignment_end])))
-    #test = pysam.faidx("/Volumes/igm/apps/genomes/Homo_sapiens/human_g1k_v37_decoy/human_g1k_v37_decoy.fasta", f"{chr}:{start}-{stop}")
-    #print(list(repetitions(test.split('\n')[1])))
+        length_list = ['N-10', 'N-9', 'N-8', 'N-7', 'N-6', 'N-5', 'N-4', 'N-3', 'N-2', 'N-1', 'N',
+                       'N+1', 'N+2', 'N+3', 'N+4', 'N+5', 'N+6', 'N+7', 'N+8', 'N+9', 'N+10']
+        repeat_count_list = []
+        for x in range(largest_rep_len - 10, largest_rep_len + 11):
+            if not length_dict[x]:
+                length_dict[x] = 0
+            repeat_count_list.append(length_dict[x])
 
-        #print(read.cigartuples)
-        #if len(read.cigartuples) > 3:
-            #print(read.cigartuples)
-        #print(read.query_alignment_start)
-        #print(read.query_alignment_end)
-        #print(read.query_alignment_length)
-        #print(read.reference_start)
-        #print(read.cigartuples[0][0])
-    #pysam.view(catch_stdout=False)
-    #pysam.view("-q", "1", "-F", "1028", args.bam, "2:47641119-47641856")
-    #iter = bam_file.fetch("2",47641419, 47641556)
-    #for x in iter:
-     #   print(str(x))
+        df['Repeat_Length'] = length_list
+        df[f'{chr}:{start}-{stop}'] = repeat_count_list
+            #print(f'rep len: {x}, nubmer of reads:{length_dict[x]}')
+
+    df.to_csv('test.csv', index=False)
 
 
 if __name__ == '__main__':
