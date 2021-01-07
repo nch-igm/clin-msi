6d5d34037e99778a4685caac272ab265dfe45cf1
Patrick Brennan <patrick.brennan@nationwidechildrens.org>
2021-01-07 14:53:29 -0500

    IGMBFX-1340 formattingn changes

--- src/msi_read_count_v2.py
+++ src/msi_read_count_v2.py
@@ -7,11 +7,12 @@ import pandas as pd
 
 def clin_msi_argparser():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument('--input-file', type=str, required=True)
-    parser.add_argument('--bam', type=str, required=True)
-    parser.add_argument('--reference', type=str, required=True)
+    parser.add_argument('--input-file', type=str, required=True, help="path to the tab separated input file")
+    parser.add_argument('--bam', type=str, required=True, help="path to BAM file")
+    parser.add_argument('--reference', type=str, required=True, help="path to reference(.fa or .fasta) file with index (.fai) in the same directory")
+    parser.add_argument('--sample-name', type=str, required=True)
     parser.add_argument('--output-dir', type=str, required=True, help="")
-    parser.add_argument('--allow_mismatch', action='store_true')
+    parser.add_argument('--allow-mismatch', action='store_true', help="allows a single base mismatch within the repeat region")
 
     return parser
 
@@ -36,24 +37,22 @@ def clin_msi():
 
     msi_location_list = parse_input_file(args.input_file)
 
+    #Load BAM file
     bam_file = pysam.AlignmentFile(args.bam, "rb")
     df = pd.DataFrame()
 
     for chr, start, stop in msi_location_list:
+        #get expected repeat length from provided reference file
         length_dict = defaultdict(int)
         fasta_seq = pysam.faidx(args.reference, f"{chr}:{start-10}-{stop+10}").split('\n')[1]
         rep_list = list(repetitions(fasta_seq))
-        print(rep_list)
         largest_rep_unit = max(rep_list, key=itemgetter(1))[0]
         largest_rep_len = int(max(rep_list, key=itemgetter(1))[1])
-        print(largest_rep_len, largest_rep_unit)
         get_flanks = re.search(fr"(\w{{5}}){largest_rep_unit}{{{largest_rep_len}}}(\w{{5}})", fasta_seq)
         left_flank = get_flanks.group(1)
         right_flank = get_flanks.group(2)
 
-        print(left_flank, fasta_seq, right_flank)
-
-
+        #Don't count reads that are unmapped, duplicate, qual=0
         for read in bam_file.fetch(chr, start-500, stop+500):
             if read.is_duplicate:
                 continue
@@ -64,14 +63,20 @@ def clin_msi():
             if read.reference_start > start:
                 continue
 
-            difference = start - read.reference_start
-            #print(re_string)
+            #difference = start - read.reference_start
+
             read_wo_softclip = read.query_sequence[read.query_alignment_start:read.query_alignment_end]
-            get_rep = re.search(fr"{left_flank}({largest_rep_unit}+){right_flank}", read_wo_softclip)
+
+            #allow one mismatched base in repeat region if specified
             if args.allow_mismatch:
                 get_rep = re.search(fr"{left_flank}({largest_rep_unit}+[ACTG]?{largest_rep_unit}+){right_flank}", read_wo_softclip)
+            else:
+                get_rep = re.search(fr"{left_flank}({largest_rep_unit}+){right_flank}", read_wo_softclip)
+
+            # if repeat is not found, go to next read
             if get_rep is None:
                 continue
+
             length_dict[len(get_rep.group(1))] += 1
 
         length_list = ['N-10', 'N-9', 'N-8', 'N-7', 'N-6', 'N-5', 'N-4', 'N-3', 'N-2', 'N-1', 'N',
@@ -84,10 +89,8 @@ def clin_msi():
 
         df['Repeat_Length'] = length_list
         df[f'{chr}:{start}-{stop}'] = repeat_count_list
-            #print(f'rep len: {x}, nubmer of reads:{length_dict[x]}')
 
-    df.to_csv('test.csv', index=False)
-
+    df.to_csv('test_with_mismatch.csv', index=False)
 
 if __name__ == '__main__':
     clin_msi()
